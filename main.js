fffbec56a9b2928cb2b4cb02b466e05bc98ac1e0
Owasimul <owasimuls@gmail.com>
2022-08-05 12:14:20 -0400

    stuff

---
+++ main.js
@@ -0,0 +1,24 @@
+let barItem = document.getElementsByClassName('barItem');
+barItem.onmouseover = function () {
+    barItem.style.backgroundColor = '#FFFFFF';
+}
+
+//contact
+const contactInfo = [
+    {
+        'name':'Github',
+        'link':'https://github.com/OwasimulS',
+        'img':'Pictures/github.png'
+    }, {
+        'name':'LinkedIn',
+        'link':'https://www.linkedin.com/in/owasimul-sidick-146373239/',
+        'img':'Pictures/linkedIn.png'
+    }, {
+        'name':'Gmail',
+        'email':'owasimuls@gmail.com',
+        'img':'Pictures/gmail.png'
+    }, {
+        'name':'PhoneNum',
+        'num':'3472578954',
+    }
+]
